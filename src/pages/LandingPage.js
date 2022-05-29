13dce489bf42b761f2f448ae69a90cd87156dad9
Muhammad Dzulfiqar Firdaus <mdzulfirdaus02@gmail.com>
2022-05-30 06:47:38 +0700

    initial updates

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Header from "parts/header";
+
+export default class LandingPage extends Component{
+    render(){
+        return(
+            <>
+                <Header {...this.props}> </Header>
+            </>
+        )
+    }
+}
